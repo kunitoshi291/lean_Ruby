0cbbebd7c60a62cacf7005a441bb1fba40cf6ab2
Takekuni <takevadogu@outlook.jp>
2022-08-14 20:52:47 +0900

    ハッシュの中のハッシュの書き方と、inspectメソッドについてを学習

--- Hash/hash.rb
+++ Hash/hash.rb
@@ -15,3 +15,21 @@ a = {'x' =>1, 'y' => 2, 'z' => 3}
 b = {'x' => 1, 'y' => 2, 'z' => 3}
 a == b
 p a
+
+# ハッシュの中のハッシュ
+params = {} # 'params' というハッシュを定義する ('parameters' の略)。
+
+params[:user] = {name: "Kenta Takahashi", email: "takahashi@example.com"}
+p params
+p params[:user][:email]
+
+# 配列や範囲オブジェクトと同様、ハッシュもeachメソッドに応答できる
+# :successと:dangerという2つの状態を持つflashという名前のハッシュ
+flash = {success: "It worked.", dsanger: "It failed."}
+flash.each do |key, value|
+puts "Key #{key.inspect} has value #{value.inspect}"
+end
+
+# inspectメソッド→ オブジェクトを人間が読める形式に変換した文字列を返す。
+[1, 2, 3, 4,'five'].inspect # => "[1, 2, 3..4, \"five\"]"
+Time.new.inspec # => "2022-08-14 20:37:15.697029 +0900"
